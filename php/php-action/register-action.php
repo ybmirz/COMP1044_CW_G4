fa7538ee80b69105fbf5c85d13311f9371986d3f
xenonial98 <mirza898@gmail.com>
2022-04-22 18:13:57 +0800

    Changed slashes

--- php/php-action/register-action.php
+++ php/php-action/register-action.php
@@ -49,7 +49,7 @@ switch ($_POST["account_type"]) {
 }
 
 // Registers
-require_once("\classes\User.php");
+require_once("/classes/User.php");
 $user = new User();
 $success = $user->processRegister(
     $owa,
